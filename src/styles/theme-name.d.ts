532ebdbfa6f642e524803aadbc02b0b0782273d6
phongit.ton <phongit.ton@gmail.com>
2025-07-22 19:40:43 +0700

    feat: add color scale to PaletteColor type

--- src/styles/theme-name.d.ts
+++ src/styles/theme-name.d.ts
@@ -22,4 +22,28 @@ declare module "@mui/material/styles" {
     yellowOpa?: Record<number, string>;
     lightGray?: Record<number, string>;
   }
+  interface PaletteColor {
+    50?: string;
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+    600?: string;
+    700?: string;
+    800?: string;
+    900?: string;
+  }
+  interface PaletteColorOptions {
+    50?: string;
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+    600?: string;
+    700?: string;
+    800?: string;
+    900?: string;
+  }
 }
