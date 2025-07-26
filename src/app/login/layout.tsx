13597f2c7847ed812038046cfc63648c48566d16
phongit.kha <153442976+phongit-kha@users.noreply.github.com>
2025-07-27 04:42:29 +0700

    fix(build): type auth error (#9)

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,9 @@
+import React, { Suspense } from "react";
+
+export default function SuccessLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Suspense>{children}</Suspense>;
+}
